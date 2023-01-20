a6edb0236153d2dc6fd0e0ef8ba772caeab03a20
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-20 05:42:25 -0500

    second cypress test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,5 +4,9 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-  })
+    // Finds the list item witht the text tuesday and checks it for the "Active" background color
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
 });
