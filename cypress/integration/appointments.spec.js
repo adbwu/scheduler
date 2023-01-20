bf7f6b9e6db5a35beb451c841696d0232941b59b
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-20 07:14:40 -0500

    added cypress to test interview edit

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,20 +1,43 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset")
     cy.visit("/")
       .contains("Monday");
+  });
+  xit("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
+
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
+
     cy.get("[alt='Sylvia Palmer']")
       .click();
+
     cy.contains("Save")
       .click();
+
     cy.get("[data-testid=appointment]")
-      .should("contain", "Saving...")
-      .should("contain", "Sylvia Palmer")
+      .should("contain", "Saving...");
+
+    cy.get("[data-testid=appointment]")
+      .should("contain", "Sylvia Palmer");
+
+    cy.get("[data-testid=appointment]")
       .should("contain", "Lydia Miller-Jones");
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
