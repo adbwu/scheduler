c1e2814698c853d4afe41dd991abdc23753597ba
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-01-22 23:00:55 -0500

    added comments to useApplicationData.jsx

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 import Axios from "axios";
 import { getRemainingSpotsForDay } from "helpers/selectors";
 
+//Hook that maintains the state of data related to interviews through out application
 export default function useApplicationData() {
 
+  //Maintains the state for items listed
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
@@ -12,12 +14,14 @@ export default function useApplicationData() {
     appointments: {}
   });
 
+  //Retrieves database content from API
   useEffect(() => {
     Promise.all([
     Axios.get('/api/days'),
     Axios.get('/api/appointments'),
     Axios.get('/api/interviewers')])
     .then((all) => {
+      //Sets the state for each item in object on API response
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })}, []);
 
@@ -25,68 +29,89 @@ export default function useApplicationData() {
     setState({ ...state, day });
   }
 
-  function setInterviewer(interviewer) {setState({ ...state, interviewer });
+  function setInterviewer(interviewer) {
+    setState({ ...state, interviewer });
   }
 
+  //Function to update remaining spots for day in side bar
   function updateSpots(appointments) {
     const dayIndex = state.days.findIndex(day => day.name === state.day);
     const day = state.days[dayIndex];
+    //Takes in appointments array and passes currently selected day's appointments to a help function that returns remaininingSpots
     const spots = getRemainingSpotsForDay(appointments, day);
     day.spots = spots;
+    //Sets the days state to trigger a rerender of sidebar
     const days = state.days.map(d => d.name === state.day ? d = day: d);
-    setState(prev => ({ ...prev, days}))
+    setState(prev => ({ ...prev, days}));
   }
 
+  //Takes in id and interview as props, and returns a promise that confirms that interview was booked.
   function bookInterview(id, interview) {
     return new Promise((resolve, reject) => {
+      //Creates appointment object
       const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
       };
+      //Adds appointment to current appointments object
       const appointments = {
       ...state.appointments,
       [id]: appointment
       };
+      //Updates appointments in API
       Axios.put(`/api/appointments/${id}`, {...appointment})
         .then((res) => {
+          //Sets the appointments state to trigger rerender
           setState({ ...state, appointments });
+          //Updates the remain spots in sidebar
           updateSpots(appointments);
+          //Returns promise to trigger Show visual mode
           resolve(res);
       })
         .catch((error) => {
           console.log(error.response.status);
           console.log(error.response.headers);
           console.log(error.response.data);
+          //Returns error to trigger Error visual mode
           reject(error);
       });
     });
   }
   
+  //Takes in interview id to remove interview from database and returns promise that confirms the appointment was deleted
   function cancelInterview(id) {
     return new Promise((resolve, reject) => {
+      //Creates empty appointment object
       const appointment = {
         ...state.appointments[id],
         interview: null
       };
+      //Adds empty appointment to current appointments object
       const appointments = {
         ...state.appointments,
         [id]: appointment
       };
+      //Updates appointments in API
       Axios.delete(`/api/appointments/${id}`, {appointment})
         .then((res) => {
+          //Sets the appointments state to trigger rerender
           setState({ ...state, appointments });
+          //Updates the remain spots in sidebar
           updateSpots(appointments);
+          //Returns promise to trigger Empty visual mode
           resolve(res);
       })
         .catch((error) => {
           console.log(error.response.status);
           console.log(error.response.headers);
           console.log(error.response.data);
+          //Returns error to trigger Error visual mode
           reject(error);
       });
     })
   }
 
+  //Exports functions
   return {
     state,
     setDay,
